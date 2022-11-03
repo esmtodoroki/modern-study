5e26129bdbe4b12b1796c8abd2470f437c330e2c
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2022-11-03 16:54:20 +0900

    Webアプリ開発2-1

--- webapp/Code.js
+++ webapp/Code.js
@@ -1,3 +1,15 @@
 function doGet() {
     return HtmlService.createTemplateFromFile('index').evaluate();
-  }
+}
+
+//カウンター値を保存するスプレッドシートの定義
+const SHEET_ID = '1P6GHIrw11mKkV0xcBEyLfcov9sQGygYouIq5zM2ScbA'
+const SHEET_NAME = 'counter'
+
+//画面から受け取ったカウンター値をA1セルに保存する
+function setSheetData(value) {
+  var spreadsheet = SpreadsheetApp.openById(SHEET_ID);
+  var sheet = spreadsheet.getSheetByName(SHEET_NAME);
+  var range = sheet.getRange('A1');
+  range.setValue(value);
+}
