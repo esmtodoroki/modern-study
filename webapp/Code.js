b36030f7552f3f1d9c955c5539b93616a2024e0e
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2022-12-05 21:49:50 +0900

    定数命名修正

--- webapp/Code.js
+++ webapp/Code.js
@@ -3,11 +3,11 @@ const SHEET_ID = '1P6GHIrw11mKkV0xcBEyLfcov9sQGygYouIq5zM2ScbA'
 const SHEET_NAME = 'counter'
 
 //CloudSQL接続情報
-const connectionName = 'esm-gcp-study:us-central1:modern-study'
-const databaseName = 'web_counter_esmtodoroki'
-const userName = 'esm'
-const password = 'esm'
-const url = 'jdbc:google:mysql://' + connectionName + '/' + databaseName
+const CONNECTION_NAME = 'esm-gcp-study:us-central1:modern-study'
+const DATABASE_NAME = 'web_counter_esmtodoroki'
+const USER_NAME = 'esm'
+const PASSWORD = 'esm'
+const URL = 'jdbc:google:mysql://' + CONNECTION_NAME + '/' + DATABASE_NAME
 
 function doGet() {
   var template = HtmlService.createTemplateFromFile('index');
@@ -37,7 +37,7 @@ function getSheetData() {
 
 //カウンター値保存用テーブルにレコードを新規作成する（初回登録）
 function insertNewRecord(value) {
-  var connection = Jdbc.getCloudSqlConnection(url, userName, password);
+  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
   var statement = connection.prepareStatement('INSERT INTO counter_table (id, counter) values (1, ?)');
   statement.setInt(1, value);
   statement.executeUpdate();
@@ -48,7 +48,7 @@ function insertNewRecord(value) {
 
 //カウンター値保存用テーブルのレコードを更新する
 function updateRecord(value) {
-  var connection = Jdbc.getCloudSqlConnection(url, userName, password);
+  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
   var statement = connection.prepareStatement('UPDATE counter_table SET counter=? WHERE id=1');
   statement.setInt(1, value);
   statement.executeUpdate();
