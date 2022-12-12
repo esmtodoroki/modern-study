5e77288dc072ba2d457cae52735a0100dc8118cc
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2022-12-12 11:23:47 +0900

    レコード数カウント処理改善と定数拡充

--- webapp/Code.js
+++ webapp/Code.js
@@ -8,6 +8,8 @@ const DATABASE_NAME = 'web_counter_esmtodoroki'
 const USER_NAME = 'esm'
 const PASSWORD = 'esm'
 const URL = 'jdbc:google:mysql://' + CONNECTION_NAME + '/' + DATABASE_NAME
+const CONNECTION = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD)
+const EXECUTE_QUERY = CONNECTION.createStatement().executeQuery
 
 function doGet() {
   var template = HtmlService.createTemplateFromFile('index');
@@ -37,31 +39,27 @@ function getSheetData() {
 
 //カウンター値保存用テーブルにレコードを新規作成する（初回登録）
 function insertNewRecord(value) {
-  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
-  var statement = connection.prepareStatement('INSERT INTO counter_table (id, counter) values (1, ?)');
+  var statement = CONNECTION.prepareStatement('INSERT INTO counter_table (id, counter) values (1, ?)');
   statement.setInt(1, value);
   statement.executeUpdate();
   
   statement.close();
-  connection.close();
+  CONNECTION.close();
 }
 
 //カウンター値保存用テーブルのレコードを更新する
 function updateRecord(value) {
-  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
-  var statement = connection.prepareStatement('UPDATE counter_table SET counter=? WHERE id=1');
+  var statement = CONNECTION.prepareStatement('UPDATE counter_table SET counter=? WHERE id=1');
   statement.setInt(1, value);
   statement.executeUpdate();
 
   statement.close();
-  connection.close();
+  CONNECTION.close();
 }
 
 //カウンター値保存用テーブルのレコード（カウンター値）を取得する
 function readRecord() {
-  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
-  var statement = connection.createStatement();
-  var results = statement.executeQuery('SELECT counter FROM counter_table WHERE id = 1');
+  var results = EXECUTE_QUERY('SELECT * FROM counter_table WHERE id = 1');
   results.next();
   var currentCounter = results.getInt('counter');
   return currentCounter;
@@ -70,10 +68,8 @@ function readRecord() {
 //カウンター値保存用テーブルのレコード数を取得する
 // （初回登録の判定に使用）
 function countRecord() {
-  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
-  var statement = connection.createStatement();
-  var results = statement.executeQuery('SELECT * FROM counter_table');
-  results.last();
-  var rowNum = results.getRow();
-  return rowNum;
+  var results = EXECUTE_QUERY('SELECT COUNT(*) cnt FROM counter_table');
+  results.next();
+  var rowCount = results.getInt('cnt');
+  return rowCount;
 }
