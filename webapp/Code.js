057f07da5dec66dccb7ea6dc3990ff01cca4db1f
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2022-12-18 15:44:21 +0900

    テーブルリード用共通関数作成

--- webapp/Code.js
+++ webapp/Code.js
@@ -8,8 +8,6 @@ const DATABASE_NAME = 'web_counter_esmtodoroki'
 const USER_NAME = 'esm'
 const PASSWORD = 'esm'
 const URL = 'jdbc:google:mysql://' + CONNECTION_NAME + '/' + DATABASE_NAME
-const CONNECTION = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD)
-const EXECUTE_QUERY = CONNECTION.createStatement().executeQuery
 
 function doGet() {
   var template = HtmlService.createTemplateFromFile('index');
@@ -39,27 +37,38 @@ function getSheetData() {
 
 //カウンター値保存用テーブルにレコードを新規作成する（初回登録）
 function insertNewRecord(value) {
-  var statement = CONNECTION.prepareStatement('INSERT INTO counter_table (id, counter) values (1, ?)');
+  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
+  var sql = 'INSERT INTO counter_table (id, counter) values (1, ?)';
+  var statement = connection.prepareStatement(sql);
   statement.setInt(1, value);
   statement.executeUpdate();
   
   statement.close();
-  CONNECTION.close();
+  connection.close();
 }
 
 //カウンター値保存用テーブルのレコードを更新する
 function updateRecord(value) {
-  var statement = CONNECTION.prepareStatement('UPDATE counter_table SET counter=? WHERE id=1');
+  var connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD);
+  var sql = 'UPDATE counter_table SET counter=? WHERE id=1';
+  var statement = connection.prepareStatement(sql);
   statement.setInt(1, value);
   statement.executeUpdate();
 
   statement.close();
-  CONNECTION.close();
+  connection.close();
+}
+
+//テーブルリード用サブ関数
+function readTableSub(sql) {
+  const connection = Jdbc.getCloudSqlConnection(URL, USER_NAME, PASSWORD)
+  return connection.createStatement().executeQuery(sql)
 }
 
 //カウンター値保存用テーブルのレコード（カウンター値）を取得する
-function readRecord() {
-  var results = EXECUTE_QUERY('SELECT * FROM counter_table WHERE id = 1');
+function readCounter() {
+  var sql = 'SELECT * FROM counter_table WHERE id = 1';
+  var results = readTableSub(sql);
   results.next();
   var currentCounter = results.getInt('counter');
   return currentCounter;
@@ -68,7 +77,8 @@ function readRecord() {
 //カウンター値保存用テーブルのレコード数を取得する
 // （初回登録の判定に使用）
 function countRecord() {
-  var results = EXECUTE_QUERY('SELECT COUNT(*) cnt FROM counter_table');
+  var sql = 'SELECT COUNT(*) cnt FROM counter_table';
+  var results = readTableSub(sql);
   results.next();
   var rowCount = results.getInt('cnt');
   return rowCount;
