df516a6532c060210cfc293b342dcfceb878cb22
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2023-02-28 11:59:28 +0900

    レイアウトのレビュー指摘取り込み

--- webapp2/src/view/BookApp.vue
+++ webapp2/src/view/BookApp.vue
@@ -1,29 +1,33 @@
 <template>
   <div>
+    <v-row class="mx-3">
     書籍を検索
+    </v-row>
     <v-row>
-      <v-col cols="3">
+      <v-col cols="3" class="pt-1 pb-0">
         <v-text-field solo clearable label="書籍名" variant="outlined" width="400"></v-text-field>
       </v-col>
-      <v-col cols="2">
+      <v-col cols="2" class="pt-1 pb-0">
         <v-select solo v-bind:items="genre" label="ジャンル" width="100"></v-select>
       </v-col>
-      <v-col cols="2" class="d-flex align-left">
-        <v-btn color="light-blue lighten-1" class="ma-2 white--text" @click="searchBook"><v-icon left>mdi-folder-search</v-icon>検索</v-btn>
-        <v-btn color="blue darken-3" class="ma-2 white--text" @click="searchClear"><v-icon left>mdi-backspace</v-icon>検索条件クリア</v-btn>
+      <v-col cols="4" class="pt-1 pb-0">
+        <v-btn color="light-blue lighten-1" class="mt-2 white--text" @click="searchBook"><v-icon left>mdi-folder-search</v-icon>検索</v-btn>
+        <v-btn color="blue darken-3" class="mt-2 ml-2 white--text" @click="searchClear"><v-icon left>mdi-backspace</v-icon>検索条件クリア</v-btn>
       </v-col>
     </v-row>
-    <p>
-    <v-btn color="teal accent-2" @click="registNewItem"><v-icon left>mdi-pencil-plus</v-icon>書籍を登録</v-btn>
-    <p>
+    <v-row>
+      <v-col class="pt-0">
+        <v-btn color="teal accent-2" @click="registNewItem"><v-icon left>mdi-pencil-plus</v-icon>書籍を登録</v-btn>
+      </v-col>
+    </v-row>
     <v-data-table
       :headers="headers"
       :items="items"
       sort-by="bookId"
     >
       <template v-slot:item.actions="{ item }">
-        <v-btn small color="orange lighten-1" class="ma-2 white--text" @click="editItem(item)"><v-icon left>mdi-pencil</v-icon>編集</v-btn>
-        <v-btn small color="red accent-3" class="ma-2 white--text" @click="deleteItem(item)"><v-icon left>mdi-delete</v-icon>削除</v-btn>
+        <v-btn small color="orange lighten-1" class="mx-1 white--text" @click="editItem(item)"><v-icon left>mdi-pencil</v-icon>編集</v-btn>
+        <v-btn small color="red accent-3" class="mx-1 white--text" @click="deleteItem(item)"><v-icon left>mdi-delete</v-icon>削除</v-btn>
       </template>
     </v-data-table>
   </div>
