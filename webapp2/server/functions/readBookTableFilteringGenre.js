c8a14560d4635bdec1dbaadc1ba58ff49f573c0c
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2023-04-24 21:30:55 +0900

    SQL文記述改善

--- webapp2/server/functions/readBookTableFilteringGenre.js
+++ webapp2/server/functions/readBookTableFilteringGenre.js
@@ -1,17 +1,17 @@
 import readTableSub from './common/readTableSub'
 //書籍テーブル内の指定されたジャンルに一致するレコードを読み込んで配列へ格納
 export default function readBookTableFilteringGenre(genreId) {
-  let sql = "";
-    sql += "SELECT ";
-    sql += "A.*,";
-    sql += "B.genre_name ";
-    sql += "FROM ";
-    sql += "book_table AS A ";
-    sql += "LEFT OUTER JOIN genre_table AS B ";
-    sql += "ON A.genre_id = B.genre_id ";
-    sql += "WHERE ";
-    sql += "A.genre_id = ";
-    sql += genreId;
+  let sql = `
+    SELECT
+      A.*,
+      B.genre_name
+    FROM
+      book_table AS A
+      LEFT OUTER JOIN genre_table AS B
+        ON A.genre_id = B.genre_id
+    WHERE
+      A.genre_id = `;
+  sql += genreId;
   const results = readTableSub(sql);
   const dataArray = [];
   while (results.next()) {
