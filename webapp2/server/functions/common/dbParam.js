b86d67c2f3265ca874ef4502ec40ab4ae4b9c200
esmtodoroki <30033531+esmtodoroki@users.noreply.github.com>
2023-05-15 22:07:17 +0900

    切り出しファイルを一旦削除

--- webapp2/server/functions/common/dbParam.js
+++
@@ -1,8 +0,0 @@
-/**
- * CloudSQL接続情報
- */
-const CONNECTION_NAME = 'esm-gcp-study:us-central1:modern-study';
-const DATABASE_NAME = 'web_book_esmtodoroki';
-export const USER_NAME = 'esm';
-export const PASSWORD = 'esm';
-export const URL = 'jdbc:google:mysql://' + CONNECTION_NAME + '/' + DATABASE_NAME;
